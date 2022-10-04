3748c6c77f0fda666f10b0b6a0a3a46c19ae52c2
AngelRodriguez2003 <angelrodriguezrodriguezbotello@gmail.com>
2022-10-04 18:42:41 -0500

    afas

--- src/App.js
+++ src/App.js
@@ -4,20 +4,34 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form style={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center" }}>
+        <label for="fname">Usuario:</label>
+        <input type="text" id="fname" name="fname" />
+        <label for="lname">Contraseña:</label>
+        <input type="password" id="lname" name="lname" />
+        <label for="lname">Color:</label>
+        <input type="color" id="lname" name="lname" />
+        <label for="lname">sexo:</label>
+        <div>
+
+          <input type="radio" id="lname" name="hombre" placeholder='hombre' />
+          <label for="lname">mujer:</label>
+        </div>
+        <div>
+          <input type="radio" id="lname" name="mujer" placeholder='mujer' />
+          <label for="lname">hombre:</label>
+
+        </div>
+
+
+
+
+        <label for="lname">Last name:</label>
+        <input type="text" id="lname" name="lname" />
+        <label for="lname">Last name:</label>
+        <input type="text" id="lname" name="lname" />
+        <button>Enviar</button>
+      </form>
     </div>
   );
 }
